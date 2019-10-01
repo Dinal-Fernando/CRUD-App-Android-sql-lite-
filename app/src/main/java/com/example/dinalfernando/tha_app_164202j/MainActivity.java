d12e9e30d03f1eb5709e69eecb542b446eb89d4f
Dinal Fernando <ffffdinal@gmail.com>
2019-10-01 14:47:20 +0530

    first commit

---
+++ app/src/main/java/com/example/dinalfernando/tha_app_164202j/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.dinalfernando.tha_app_164202j;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin, btnRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btn_login);
+        btnRegister = findViewById(R.id.btn_register);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/dinalfernando/tha_app_164202j/UserProfile.java
@@ -0,0 +1,18 @@
+package com.example.dinalfernando.tha_app_164202j;
+
+public class UserProfile {
+
+    String index,
+            name,
+            email,
+            mobile,
+            gpa;
+
+    public UserProfile(String index, String name, String email, String mobile, String gpa) {
+        this.index = index;
+        this.name = name;
+        this.email = email;
+        this.mobile = mobile;
+        this.gpa = gpa;
+    }
+}
